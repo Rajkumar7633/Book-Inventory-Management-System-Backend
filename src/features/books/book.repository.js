326e85ecc6f6987cb51963b2bd21c1c05dca6e63
Raj Kumar <118206888+Rajkumar7633@users.noreply.github.com>
2024-06-21 01:28:32 +0530

    Initial commit

---
+++ src/features/books/book.repository.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import { bookSchema } from './book.schema.js'
+
+// creating model from schema.
+const booksModel = mongoose.model('Book', bookSchema);
+
+export default class BookRepository {
+    async createBook(bookData) {
+        const book = new booksModel(bookData);
+        const savedBook = await book.save();
+        return savedBook;
+    }
+
+    async getOne(id) {
+        const book = await booksModel.findById(id);
+        return book;
+    }
+
+    async listBooksByGenre(genre) {
+        const books = await booksModel.find({ genre });
+        return books;
+    }
+
+    async updateBookAvailability(bookId, quantity) {
+
+        console.log(bookId);
+        const book = await booksModel.findById(bookId);
+
+        // Calculate the new availableCopies value
+        const newAvailableCopies = book.availableCopies + quantity;
+
+        // Update the availableCopies field and save the book
+        book.availableCopies = newAvailableCopies;
+
+        await book.save();
+        return book;
+    }
+
+    async deleteBookById(bookId) {
+        const deletedBook = await booksModel.findByIdAndRemove(bookId);
+        return deletedBook;
+    }
+}
